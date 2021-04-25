54be775973eea45b5bedf877976dbcac0c5ea0ba
hemagna <hemagna.rao@gmail.com>
2021-04-25 15:01:31 +0000

    added variables

---
+++ variables/var.c
@@ -0,0 +1,24 @@
+int playerScore = 95;
+char ch = 'a';
+long a;
+long long b;
+long double c;
+/* 
+    A variable name can only have letters (both uppercase and lowercase letters), digits and underscore.
+    The first letter of a variable should be either a letter or an underscore.
+    There is no rule on how long a variable name (identifier) can be. However, you may run into problems in some compilers if the variable name is longer than 31 characters.
+The different bases are :
+Decimal: 0, -9, 22 etc
+Octal: 021, 077, 033 etc
+Hexadecimal: 0x7f, 0x2a, 0x521 etc
+"good"                  //string constant
+""                     //null string constant
+"      "               //string constant of six white space
+"x"                    //string constant having a single character.
+"Earth is round\n"         //prints string with a newline
+If you want to define a variable whose value cannot be changed, you can use the const keyword. This will create a constant
+void is an incomplete type. It means "nothing" or "no type". You can think of void as absent. you cannot create variables of void type.
+
+
+
+*/
